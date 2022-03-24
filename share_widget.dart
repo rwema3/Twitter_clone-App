12a59bd1396663c50a242bb8c3871ccbb63a1440
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-24 21:38:12 +0200

    padding: const EdgeInsets.symmetric(vertical: 16),

--- share_widget.dart
+++ share_widget.dart
@@ -92,4 +92,5 @@ class _MyHomePageState extends State<ShareWidget> {
                 key: _globalKey,
                 child: Container(
                   color: Theme.of(context).colorScheme.onPrimary,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
  
