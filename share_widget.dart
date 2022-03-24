0f247d350d6c16670a29172ef6eacd39d70e4265
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-24 21:35:31 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -51,3 +51,4 @@ class _MyHomePageState extends State<ShareWidget> {
           .findRenderObject() as RenderRepaintBoundary;
       ui.Image image = await boundary.toImage(pixelRatio: 3.0);
       ByteData? byteData =
+          await image.toByteData(format: ui.ImageByteFormat.png);
