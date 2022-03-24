d833239250dd15f559558708503166c199c69f55
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-24 21:38:50 +0200

    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),

--- share_widget.dart
+++ share_widget.dart
@@ -98,4 +98,5 @@ class _MyHomePageState extends State<ShareWidget> {
                   ),
                 )),
             Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
  
