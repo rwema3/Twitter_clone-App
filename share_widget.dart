57634d4af7089023c29df6eaf089090b8b71331d
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-24 21:34:00 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -23,3 +23,4 @@ class ShareWidget extends StatefulWidget {
       required SocialMetaTagParameters socialMetaTagParameters,
       required String id}) {
     return MaterialPageRoute(
+      builder: (_) => ShareWidget(
