d7a7cad5917e329be81179934d25b6eab334770c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 21:14:02 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -12,7 +12,7 @@ class ShareWidget extends StatefulWidget {
   const ShareWidget(
       {Key? key,
       required this.child,
-      required thi
+      required this
       : super(key: key);
 
   final SocialMetaTagParameters socialMetaTagParameters;
