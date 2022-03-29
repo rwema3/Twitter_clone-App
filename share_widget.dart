453d01a14400f97cfd4dedde6d19379f38581f56
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 20:35:18 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -78,7 +78,7 @@ class _MyHomePageState extends State<ShareWidget> {
                     child: widget.child,
                   ),
                 )),
-            Padd        fontSize: 18,
+            Paddi        fontSize: 18,
                     color: Theme.of(context).colorScheme.onPrimary,
                     fontWeight: FontWeight.bold),
               ),
