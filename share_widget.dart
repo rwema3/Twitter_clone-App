c8f11b771f43c922eca251bb0aa58eb6ffc70fc4
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 20:38:09 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -79,7 +79,7 @@ class _MyHomePageState extends State<ShareWidget> {
                   ),
                 )),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal:         fontSize: 18,
+              padding: const EdgeInsets.symmetric(horizontal: 1        fontSize: 18,
                     color: Theme.of(context).colorScheme.onPrimary,
                     fontWeight: FontWeight.bold),
               ),
