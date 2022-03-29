c71556f6a918b6c1acc5b2c172423dfb90943796
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 19:38:23 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -13,7 +13,7 @@ import 'package:path_provider/path_provider.da
   final String id;
   static MaterialPageRoute getRoute(
       {required Widget child,
-      requchild,
+      reqchild,
           id: id,
           socialMetaTagParameters: socialMetaTagParameters),
     );
