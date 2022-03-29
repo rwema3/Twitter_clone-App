5e2cab7ee40460b719432c747f5f4ba2d2d75a00
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 21:01:47 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -11,7 +11,7 @@ import 'package:path_provider/path_provider.da
 
   final SocialMetaTagParameters socialMetaTagParameters;
   final String id;
-  static MaterialPageRchild,
+  static MaterialPageRochild,
           id: id,
           socialMetaTagParameters: socialMetaTagParameters),
     );
