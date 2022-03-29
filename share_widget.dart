20f3ee22127e1656adb62b2a53d72dd2a4d7b228
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 19:51:31 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -5,7 +5,7 @@ import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_twitter_clone/helper/utility.dart';
-import 'package:flutter_twitter_clone/widgets/customFlatButton.dar
+import 'package:flutter_twitter_clone/widgets/customFlatButton.da
   final SocialMetaTagParameters socialMetaTagParametechild,
           id: id,
           socialMetaTagParameters: socialMetaTagParameters),
