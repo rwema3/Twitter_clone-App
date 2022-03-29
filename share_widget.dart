bd3ea00b0a45366e36efc9f21ee4e92b8bbef786
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 20:44:24 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -84,7 +84,7 @@ class _MyHomePageState extends State<ShareWidget> {
                 label: "Share",
                 onPressed: _capturePng,
                 isLoading: isLoading,
-                la        fontSize: 18,
+                lab        fontSize: 18,
                     color: Theme.of(context).colorScheme.onPrimary,
                     fontWeight: FontWeight.bold),
               ),
