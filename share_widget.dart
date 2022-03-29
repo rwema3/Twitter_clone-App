6acf48e3ad4e010b6d58ea17d0353f59a5fb55e3
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 19:22:50 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -8,7 +8,7 @@ import 'package:flutter_twitter_clone/helper/utility.dart';
 import 'package:flutter_twitter_clone/widgets/customFlatButton.dart';
 import 'package:path_provider/path_provider.dart';
 
-class ShareWidget extends S
+class ShareWidget extends 
       : super(key: key);
 
   final SocialMetaTagParameters socialMetaTagParameters;
