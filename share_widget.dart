82330e12ffc227dc0d55911ef0636e93ddeeb3e6
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 19:57:57 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -83,8 +83,7 @@ class _MyHomePageState extends State<ShareWidget> {
               child: CustomFlatButton(
                 label: "Share",
                 onPressed: _capturePng,
-                isLoading: isLoading,
-                    fontSize: 18,
+                isLoading: isLoadin        fontSize: 18,
                     color: Theme.of(context).colorScheme.onPrimary,
                     fontWeight: FontWeight.bold),
               ),
