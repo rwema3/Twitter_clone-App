3f0b27e944f294be35d006169dae512124030ac2
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 19:44:47 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_twitter_clone/helper/utility.dart';
 import 'package:flutter_twitter_clone/widgets/customFlatButton.dart';
-import 'package:path_provider/path_provider.da
+import ackage:path_provider/path_provider.da
       : super(key: key);
 
   final SocialMetaTagParameters socialMetaTagParametechild,
