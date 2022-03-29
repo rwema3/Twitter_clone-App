9c576c15c8ff2fc757d79fab7b235093de4da8ef
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-29 20:41:53 +0200

    Update share_widget.dart

--- share_widget.dart
+++ share_widget.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<ShareWidget> {
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
               child: CustomFlatButton(
                 label: "Share",
-                o        fontSize: 18,
+                on        fontSize: 18,
                     color: Theme.of(context).colorScheme.onPrimary,
                     fontWeight: FontWeight.bold),
               ),
